e3136cc106372ffb2761cb41a2c798cc821fb718
okumualex <alecky358425@gmail.com>
2024-09-20 20:56:11 +0300

    change not found message

--- src/components/Dashboard/LadingPage.js
+++ src/components/Dashboard/LadingPage.js
@@ -3,7 +3,7 @@ import React from 'react'
 const LadingPage = () => {
   return (
     <div>
-      <h1>This page has been suspended temporarily for more development! Will be live soon...</h1>
+      <h3>This page has been suspended temporarily! Will be live soon...!</h3>
     </div>
   )
 }
