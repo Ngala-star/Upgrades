bd4bef2746b8c1c4938cb83b0b031a8589df853f
okumualex <alecky358425@gmail.com>
2024-09-20 20:15:05 +0300

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import LadingPage from "./components/Dashboard/LadingPage";
 import Home from "./components/home/Home";
 import "./styles.css";
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 export default function App() {
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter>
+      <Routes>
+        <Route path="/home" element={<LadingPage/>}/>
+        <Route path="/view" element={<Home/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Dashboard/LadingPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LadingPage = () => {
+  return (
+    <div>
+      <h1>This page has been suspended temporarily for more development! Will be live soon...</h1>
+    </div>
+  )
+}
+
+export default LadingPage
