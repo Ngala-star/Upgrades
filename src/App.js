2a448d5d5172283b51138f400ff90f2a3f7038e5
okumualex <alecky358425@gmail.com>
2024-10-07 14:43:42 +0300

    remove landing page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import LadingPage from "./components/Dashboard/LadingPage";
 import Home from "./components/home/Home";
 import "./styles.css";
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+
 
 export default function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LadingPage/>}/>
-        <Route path="/view" element={<Home/>}/>
-      </Routes>
-      </BrowserRouter>
+      <Home/>
     </div>
   );
 }
